1d692a046d9700ff90d1df09b7edfd86a752ae98
John-Coding394 <johnchaghouri394@gmail.com>
2025-04-26 10:15:47 -0400

    First commit

---
+++ activity1.py
@@ -0,0 +1,7 @@
+m = 40
+s = 70
+h = 50
+e = 60
+total = m + s + h + e 
+avg = total/4
+print(avg)
---
+++ main.py
@@ -0,0 +1,9 @@
+t1 = 98
+t2 = 94
+t3 = 41
+t4 = 96
+t5 = 11
+
+sum = t1 + t2 + t3 + t4 + t5
+div = sum/5
+print(div)
